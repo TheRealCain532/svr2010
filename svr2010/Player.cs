6fae1173ca2bd8809eac1fe33f2a726efdeef7b5
TheRealCain532 <brandoneprintiss@gmail.com>
2020-09-04 12:36:06 -0500

    Add project files.

---
+++ svr2010/Player.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace svr2010
+{
+    public class Player
+    {
+        private uint p;
+        private fEdit fs;
+        public byte Strength { get =>  fs.ReadByte(p); set  => fs.WriteByte(p, value); }
+        public byte Submission { get =>  fs.ReadByte(p + 1); set  => fs.WriteByte(p + 1, value); }
+        public byte Speed { get =>  fs.ReadByte(p + 2); set  => fs.WriteByte(p + 2, value); }
+        public byte Technique { get =>  fs.ReadByte(p + 3); set  => fs.WriteByte(p + 3, value); }
+        public byte Durability { get =>  fs.ReadByte(p + 4); set  => fs.WriteByte(p + 4, value); }
+        public byte Charisma { get =>  fs.ReadByte(p + 5); set  => fs.WriteByte(p + 5, value); }
+        public byte Stamina { get =>  fs.ReadByte(p + 6); set  => fs.WriteByte(p + 6, value); }
+        public byte Hardcore { get =>  fs.ReadByte(p + 7); set  => fs.WriteByte(p + 7, value); }
+        public byte[] Stats { get => new byte[] { Strength, Submission, Speed, Technique, Durability, Charisma, Stamina, Hardcore }; set => fs.WriteBytes(p, value); }
+        public byte MaxStrength { get =>  fs.ReadByte(p + 8); set  => fs.WriteByte(p + 8, value); }
+        public byte MaxSubmission { get =>  fs.ReadByte(p + 9); set  => fs.WriteByte(p + 9, value); }
+        public byte MaxSpeed { get =>  fs.ReadByte(p + 10); set  => fs.WriteByte(p + 10, value); }
+        public byte MaxTechnique { get =>  fs.ReadByte(p + 11); set  => fs.WriteByte(p + 11, value); }
+        public byte MaxDurability { get => fs.ReadByte(p + 12); set => fs.WriteByte(p + 12, value); }
+        public byte MaxCharisma { get => fs.ReadByte(p + 13); set => fs.WriteByte(p + 13, value); }
+        public byte MaxStamina { get =>  fs.ReadByte(p + 14); set  => fs.WriteByte(p + 14, value); }
+        public byte MaxHardcore { get =>  fs.ReadByte(p + 15); set  => fs.WriteByte(p + 15, value); }
+        public byte[] MaxStats { get => new byte[] { MaxStrength, MaxSubmission, MaxSpeed, MaxTechnique, MaxDurability, MaxCharisma, MaxStamina, MaxHardcore }; set => fs.WriteBytes(p + 8, value); }
+        public string NameText { get => fs.ReadString(p + 34); set => fs.WriteString(p + 34, value); }
+        public string HUDText { get => fs.ReadString(p + 102); set => fs.WriteString(p + 102, value); }
+        public string Nickname { get => fs.ReadString(p + 170); set => fs.WriteString(p + 170, value); }
+        public Player(string filename, uint start = 0x0002ACD8) { fs = new fEdit(filename); p = start; }
+    }
+
+}
---
+++ svr2010/mem.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace svr2010
+{
+    class fEdit
+    {
+        private string fName;
+        public byte[] ReadBytes(uint address, int length)
+        {
+            var buff = new byte[length];
+            Stream fs = File.OpenRead(fName);
+            BinaryReader b = new BinaryReader(fs);
+            b.BaseStream.Seek(address, SeekOrigin.Begin);
+            b.Read(buff, 0, length);
+            b.Close();
+            fs.Close();
+            return buff;
+        }
+        public string ReadString(uint offset)
+        {
+            int length = 40;
+            int num2 = 0;
+            string source = "";
+            do
+            {
+                byte[] bytes = this.ReadBytes(offset + ((uint)num2), length);
+                source = source + Encoding.UTF8.GetString(bytes);
+                num2 += length;
+            }
+            while (!source.Contains<char>('\0'));
+            int index = source.IndexOf('\0');
+            string str2 = source.Substring(0, index);
+            source = string.Empty;
+            return str2;
+        }
+        public byte ReadByte(uint offset) => ReadBytes(offset, 1)[0];
+        public void WriteByte(uint offset, byte data) => WriteBytes(offset, new byte[] { data });
+        public void WriteBytes(uint offset, byte[] data)
+        {
+            FileStream fs = File.OpenWrite(fName);
+            BinaryWriter b = new BinaryWriter(fs);
+            fs.Position = offset;
+            b.Write(data);
+            b.Close();
+            fs.Close();
+        }
+        public void WriteString(uint offset, string data) => WriteBytes(offset, ASCIIEncoding.ASCII.GetBytes(data));
+        public fEdit(string file) => fName = file;
+    }
+}
